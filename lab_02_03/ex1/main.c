a21ac7013bb68b39476b94dc5e8997ffc0751c45
kam16u810 <bokseng12@gmail.com>
2017-03-28 11:57:19 +0300

    Corrections have been added.

--- lab_02_03/ex1/main.c
+++ lab_02_03/ex1/main.c
@@ -6,19 +6,22 @@ int main()
 {
     printf("Input a, b nums, separated with space: ");
     int a, b;
-    scanf("%d%d", &a, &b);
+    if (scanf("%d%d", &a, &b) != 0)
+	{
+		while (a * b != 0)
+		{
+			if (a > b)
+				a = a % b;
+			else
+				b = b % a;
+		}
 
-    while (a * b != 0)
-    {
-        if (a > b)
-            a = a % b;
-        else
-            b = b % a;
-    }
-
-    if (a != 0)
-        printf("NOD result: %d", a);
-    else
-        printf("Nod result: %d", b);
+		if (a != 0)
+			printf("NOD result: %d", a);
+		else
+			printf("Nod result: %d", b);
+	}
+	else
+		printf("Error!Check your input.");
     return 0;
 }
--- lab_02_03/ex3/main.c
+++ lab_02_03/ex3/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#define index 3
 
 float f(float x);
 
@@ -7,19 +8,21 @@ int main()
 {
     printf("input x and eps(separated with space): ");
     float x, eps;
-    scanf("%f%f", &x, &eps);
-
-    float s = x;
-    int index = 3;
-    while ( fabs(s > eps) )
-    {
-        s -= pow(x, index)/index;
-        index += 2;
-    }
-    printf("S(x) result: %.3f\n", s);
-    printf("f(x) result: %.3f\n\n", f(x));
-    printf("Absolute error: %.3f\n", fabs( f(x) - s) );
-    printf("Relative error: %.3f", fabs( (f(x) - s) / f(x)) );
+    if (scanf("%f%f", &x, &eps) && eps < 1.0)
+	{
+		float s = x;
+		while ( fabs(s > eps) )
+		{
+			s -= pow(x, index)/index;
+			index += 2;
+		}
+		printf("S(x) result: %.3f\n", s);
+		printf("f(x) result: %.3f\n\n", f(x));
+		printf("Absolute error: %.3f\n", fabs( f(x) - s) );
+		printf("Relative error: %.3f", fabs( (f(x) - s) / f(x)) );
+	}
+	else
+		printf("Error!Check your input.");
 }
 
 float f(float x)
