86db498bf4e5d2a736077f7c991dc85d5e198db9
kam16u810 <bokseng12@gmail.com>
2017-05-01 22:27:42 +0300

    new functions has been added in program

--- lab_05/lab_05_04/main.c
+++ lab_05/lab_05_04/main.c
@@ -2,7 +2,9 @@
 
 void insertions_sort(int *a, int n);
 
-void array_init(FILE *f);
+int size_of_array(FILE *f);
+
+void array_init(int *pa, FILE *f, int size);
 
 int main()
 {
@@ -10,11 +12,41 @@ int main()
     char string_array[10] = "";
     char *file_name = gets(string_array);
     FILE *f = fopen(file_name, "r");
-    array_init(f);
+    int size = size_of_array(f);
+    int a[size];
+    int *pa = a;
+    array_init(pa, f, size);
+    for (int i = 1; i < size; i++)
+    {
+        printf("%d\n", a[i]);
+    }
+    fclose(f);
     return 0;
 }
 
-array_init(FILE *f)
+int size_of_array(FILE *f)
 {
+    int counter = 0;
+    int num;
+    while ( !feof(f) )
+    {
+        if ( fscanf( f, "%d", &num ) )
+            counter += 1;
+    }
+    return counter;
+}
 
+void array_init(int *pa, FILE *f, int size)
+{
+    fseek(f, 0, SEEK_SET);
+    int num;
+    int i = 0;
+    while ( (!feof(f)) && (i != size) )
+    {
+        if ( fscanf( f, "%d", &num ) )
+        {
+            *pa = num;                                      //Dont work!
+            i++;
+        }
+    }
 }
