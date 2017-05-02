07a90f01612fdb5033b997f2a71b91e1b3ecbbfe
kam16u810 <bokseng12@gmail.com>
2017-05-02 18:54:16 +0300

    Fin version of program.Global changes

--- lab_05/lab_05_04/main.c
+++ lab_05/lab_05_04/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void swap(int* a, int* b);
+
 void insertions_sort(int *pa, int* pa_fin);
 
 int* array_init(int *pa, FILE *f, int* code);
@@ -17,11 +19,18 @@ int main()
     int* pa_fin = array_init(pa, f, &code);
     if ( !code )
     {
-        for (pa = a; pa < pa_fin - 1; pa++)
+        insertions_sort(a, pa_fin);
+        int counter = 0;
+        for (pa = a + 1; pa < pa_fin - 1; pa++)
         {
-            printf("+++");
-            printf("%d\n", *pa);
+            if ( *pa != *(pa - 1) && *pa != *(pa + 1) )
+                counter++;
         }
+        if ( *a != *(a + 1) )
+            counter++;
+        if ( *(pa_fin - 1) != *(pa_fin - 2) )
+            counter++;
+        printf("Result: %d", counter);
     }
     else
         printf("Error!");
@@ -46,10 +55,27 @@ int* array_init(int *pa_fin, FILE *f, int* code)
             stop = 1;
         }
     }
-    return ++pa_fin;
+    return pa_fin;
 }
 
-void insertions_sort(int* pa, int* pa_fin)
+void swap(int* a, int* b)
 {
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
 
+void insertions_sort(int* a, int* pa_fin)
+{
+    int* pa = a;
+    for (pa = a + 1; pa < pa_fin; pa++)
+    {
+        for (int* i= pa; i > a - 1; i--)
+        {
+            if (*(i-1) > *i)
+            {
+                swap(i-1, i);
+            }
+        }
+    }
 }
