334a63e565c035a4ed2da10dd4644a815ee3453d
kam16u810 <bokseng12@gmail.com>
2017-05-25 02:24:01 +0300

    Makefile has been added, modules released

---
+++ lab_06/insertions_sort.h
@@ -0,0 +1,7 @@
+#ifndef __INSERTIONS_SORT_H__
+#define __INSERTIONS_SORT_H__
+#include <stdio.h>
+
+void insertions_sort(int *pa, int* pa_fin);
+
+#endif
--- lab_06/main.c
+++ lab_06/main.c
@@ -1,15 +1,12 @@
 #include "swap.h"
+#include "insertions_sort.h"
+#include "uniq_nums_counter.h"
+#include "array_init.h"
 #include <stdio.h>
 
 #define size_of_array 100
 
 #define size_of_string 10
-
-void insertions_sort(int *pa, int* pa_fin);
-
-int* array_init(int *pa, FILE *f, int* code);
-
-int uniq_nums_counter(int* pa, int* pa_fin, int* a);
 
 int main()
 {
@@ -34,52 +31,3 @@ int main()
     return 0;
 }
 
-int* array_init(int *pa_fin, FILE *f, int* code)
-{
-    int num;
-    int stop = 0;
-    while ( !feof(f) && !stop)
-    {
-        if ( fscanf( f, "%d", &num ) )
-        {
-            *pa_fin = num;
-            pa_fin++;
-        }
-        else
-        {
-            *code = 1;
-            stop = 1;
-        }
-    }
-    return pa_fin;
-}
-
-void insertions_sort(int* a, int* pa_fin)
-{
-    int* pa = a;
-    for (pa = a + 1; pa < pa_fin; pa++)
-    {
-        for (int* pa_i= pa; pa_i > a - 1; pa_i--)
-        {
-            if (*(pa_i-1) > *pa_i)
-            {
-                swap(pa_i-1, pa_i);
-            }
-        }
-    }
-}
-
-int uniq_nums_counter(int *pa, int *pa_fin, int* a)
-{
-    int counter = 0;
-    for (pa = a + 1; pa < pa_fin - 1; pa++)
-    {
-        if ( *pa != *(pa - 1) && *pa != *(pa + 1) )
-            counter++;
-    }
-    if ( *a != *(a + 1) )
-        counter++;
-    if ( *(pa_fin - 1) != *(pa_fin - 2) )
-        counter++;
-    return counter;
-}
