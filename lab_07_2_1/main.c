eed34be15efc600c2881423577d92cdda6f6f5b1
kam16u810 <bokseng12@gmail.com>
2017-09-06 23:30:50 +0300

    Type filter was added

--- lab_07_2_1/main.c
+++ lab_07_2_1/main.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define start_size 100
-#define size_of_string 10
+
+int len_array_int(FILE *f, int* code);
+int len_array_double(FILE *f, int* code);
 
 int* array_init(int *pa_fin, FILE *f, int* code);
 void array_print(int *pa, int *pa_fin);
+
 
 int main(int argc, char **argv)
 {
     if (argc > 1)
     {
-        printf("%c", argv[1]);
         FILE *f = fopen(argv[1], "r");
 
-        int a[start_size];
-        int* pa = a;
         int code = 0;
+        int type = 0;                //Type of varAr (int)
 
-        printf("\n\n+\n\n");
-        int* pa_fin = array_init(pa, f, &code);
-        printf("\n\n+\n\n");
+        int len = len_array_int(f, &code);
+        if (code)
+        {
+            type = 1;
+            code = 0;
+            rewind(f);
+            len = len_array_double(f, &code);
+        }
         if ( !code )
         {
-            array_print(pa, pa_fin);
-            printf("\nResult: +\n\n");
+            printf("%d", len);
+            // Ввести здесь расширение на динамическиймиссив
+            // Prog
         }
         else
-            printf("\nError!\n");
+            printf("\nFile reading error!\n");
         fclose(f);
     }
     else
@@ -34,18 +41,54 @@ int main(int argc, char **argv)
     return 0;
 }
 
-int* array_init(int *pa_fin, FILE *f, int* code)
+int len_array_int(FILE *f, int* code)
 {
     int stop = 0;
     int count = 0;
+    int empty;
+    while ( !feof(f) && !stop)
+    {
+        if ( fscanf( f, "%d", &empty ) )
+        {
+            count++;
+        }
+        else
+        {
+            *code = 1;
+            stop = 1;
+        }
+    }
+    return count;
+}
+
+int len_array_double(FILE *f, int* code)
+{
+    int stop = 0;
+    int count = 0;
+    double empty;
+    while ( !feof(f) && !stop)
+    {
+        if ( fscanf( f, "%lf", &empty ) )
+        {
+            count++;
+        }
+        else
+        {
+            *code = 1;
+            stop = 1;
+        }
+    }
+    return count;
+}
+
+int* array_init(int *pa_fin, FILE *f, int* code)
+{
+    int stop = 0;
     while ( !feof(f) && !stop)
     {
         if ( fscanf( f, "%d", pa_fin ) )
         {
-            printf("\n%d\n", *pa_fin);
             pa_fin++;
-            count++;
-            printf("\n%d\n", count);
         }
         else
         {
@@ -59,5 +102,5 @@ int* array_init(int *pa_fin, FILE *f, int* code)
 void array_print(int *pa, int *pa_fin)
 {
     for (int* pa_i = pa; pa_i < pa_fin; pa_i++)
-            printf("%d+ ", *pa_i);
+            printf("%d ", *pa_i);
 }
