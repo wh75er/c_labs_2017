d673e03d2d1f6d43921db1670f4cd0fbf4921188
kam16u810 <bokseng12@gmail.com>
2017-09-12 07:03:20 +0300

    Final version of program was added

--- lab_07_2_1/main_debug.c
+++ lab_07_2_1/main_debug.c
@@ -5,6 +5,11 @@
 int len_array_int(FILE *f, int* code);
 int len_array_double(FILE *f, int* code);
 
+//print and init
+int* array_init(int *pa_fin, FILE *f, int* code);
+void array_print_int(int *pa, int *pa_fin);
+void array_print_double(double *pa, double *pa_fin);
+
 //dynamic array creation
 int* dynamic_array_int(FILE* f, int len, int* code);
 double* dynamic_array_double(FILE* f, int len, int* code);
@@ -18,7 +23,7 @@ double* filter_double(double* a, int* len);
 void bubble_sort(void* array, size_t nmemb, size_t type);
 
 //compare
-char compare(const void* a, const void* b, size_t type);
+char compare(const void* a1, const void* b1, size_t type);
 
 //swap
 void swap(void *a1, void *b1, size_t type);
@@ -43,13 +48,20 @@ int main(int argc, char **argv)
         }
         if ( !code )
         {
+            printf("\n%d\n", type);
+            printf("\n%d\n", len);
             if (type == 4)                      //int branch work
             {
                 int* array = dynamic_array_int(f, len, &code);
+                array_print_int(array, array+len);
+                printf("%c", argv[3]);
                 if (*argv[3] == '1')
                     array = filter_int(array, &len);
+                char flag = compare((array+2), (array+2), type);
+                printf("\n %d %c %d\n", *(array+2), flag, *(array+2));
 
                 bubble_sort(array, len, type);
+                array_print_int(array, array+len);
 
                 FILE *fo = fopen(argv[2], "w");
                 for(int* pa = array; pa < array + len; pa++)
@@ -60,12 +72,14 @@ int main(int argc, char **argv)
             else                                //double branch work
             {
                 double* array = dynamic_array_double(f, len, &code);
+                array_print_double(array, array+len);
                 if (*argv[3] == '1')
                     array = filter_double(array, &len);
-                char flag = compare( (array), array +  2, type);
-                printf("\n%lf %c %lf", *(array), flag, *(array + 2));
+                char flag = compare((array+1), array, type);
+                printf("\n %lf %c %lf\n", *(array+1), flag, *array);
 
                 bubble_sort(array, len, type);
+                array_print_double(array, array+len);
 
                 FILE *fo = fopen(argv[2], "w");
                 for(double* pa = array; pa < array + len; pa++)
@@ -123,6 +137,37 @@ int len_array_double(FILE *f, int* code)
         }
     }
     return count;
+}
+
+//print and init
+int* array_init(int *pa_fin, FILE *f, int* code)
+{
+    int stop = 0;
+    while ( !feof(f) && !stop)
+    {
+        if ( fscanf( f, "%d", pa_fin ) )
+        {
+            pa_fin++;
+        }
+        else
+        {
+            *code = 1;
+            stop = 1;
+        }
+    }
+    return pa_fin;
+}
+
+void array_print_int(int *pa, int *pa_fin)
+{
+    for (int* pa_i = pa; pa_i < pa_fin; pa_i++)
+            printf("%d ", *pa_i);
+}
+
+void array_print_double(double *pa, double *pa_fin)
+{
+    for (double* pa_i = pa; pa_i < pa_fin; pa_i++)
+            printf("%lf ", *pa_i);
 }
 
 //dynamic array creation
@@ -185,6 +230,7 @@ int* filter_int(int* a, int* len)
         if (*pa < *min)
             min = pa;
     }
+    printf("\n%d, %d, %d\n", *min, *max, (max - min - 1));
     if ((max - min - 1) > 0)
     {
         *len = (max - min - 1);
@@ -195,6 +241,7 @@ int* filter_int(int* a, int* len)
             *fpa = *pa;
             fpa++;
         }
+        array_print_int(farray, farray+(max - min - 1));
 
         return farray;
     }
@@ -208,10 +255,12 @@ int* filter_int(int* a, int* len)
             *fpa = *pa;
             fpa++;
         }
+        array_print_int(farray, farray+(min - max - 1));
 
         return farray;
     }
 
+    printf("\n%d, %d, %d\n", *min, *max, (max - min - 1));
     return a;
 }
 
@@ -237,6 +286,7 @@ double* filter_double(double* a, int* len)
             *fpa = *pa;
             fpa++;
         }
+        array_print_double(farray, farray+(max - min - 1));
         return farray;
     }
   else if ((min - max - 1) > 0)
@@ -249,9 +299,11 @@ double* filter_double(double* a, int* len)
             *fpa = *pa;
             fpa++;
         }
+        array_print_double(farray, farray+(min - max - 1));
         return farray;
     }
 
+    printf("\n%lf, %lf, %d\n", *min, *max, (max - min - 1));
     return a;
 }
 
@@ -265,7 +317,8 @@ void bubble_sort(void *array, size_t nmemb, size_t type)
         {
             for (int* pa = (int*)array; pa < (int*)(array + (nmemb - 1 - n) * type); pa++)
             {
-                if (compare((pa+1), pa, type) == '<')
+                printf("%d\n", *pa);
+                if (*(pa + 1) < *pa)
                     swap((pa+1), pa, type);
             }
             n++;
@@ -279,7 +332,8 @@ void bubble_sort(void *array, size_t nmemb, size_t type)
         {
             for (double* pa = (double*)array; pa < (double*)(array + (nmemb - 1 - n) * type); pa++)
             {
-                if (compare((pa+1), pa, type) == '<')
+                printf("%lf\n", *pa);
+                if (*(pa + 1) < *pa)
                     swap((pa+1), pa, type);
             }
             n++;
@@ -288,21 +342,18 @@ void bubble_sort(void *array, size_t nmemb, size_t type)
 }
 
 //compare
-char compare(const void *a, const void *b, size_t type)
+char compare(const void *a1, const void *b1, size_t type)
 {
-    if (type == 4)
+    char* a = (char*)(a1+type-1);
+    char* b = (char*)(b1+type-1);
+    for(int i = 0; i < type; i++)
     {
-        if (*(int*)a > *(int*)b)
+        if (*a > *b)
             return '>';
-        if (*(int*)a < *(int*)b)
+        if (*a < *b)
             return '<';
-    }
-    if (type == 8)
-    {
-        if (*(double*)a > *(double*)b)
-            return '>';
-        if (*(double*)a < *(double*)b)
-            return '<';
+        a--;
+        b--;
     }
     return '=';
 }
