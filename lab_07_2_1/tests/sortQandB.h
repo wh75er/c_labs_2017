98291d514159520117e1bb5660cf1831f3512a9e
kam16u810 <bokseng12@gmail.com>
2017-09-20 08:28:35 +0300

    Time cmp has been created

--- lab_07_2_1/tests/sortQandB.h
+++ lab_07_2_1/tests/sortQandB.h
@@ -2,5 +2,14 @@
 #define __TIMER__
 
 #include "../libs.h"
+#include "../counter.h"
+#include "../dynamic_arrays.h"
+#include "../swap.h"
+#include "../compare.h"
+#include "../sort_filter.h"
+
+#include "../file_open.h"
+#include "../main_dynamic_array.h"
+#include <string.h>
 
 #endif
