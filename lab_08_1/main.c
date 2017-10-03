d26ca82c7f8eee0d9a088b79d71f572376a25d32
kam16u810 <bokseng12@gmail.com>
2017-10-03 22:09:12 +0300

    Two functions have been added

--- lab_08_1/main.c
+++ lab_08_1/main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define OK 0
-#define FILE_OPEN_ERROR -1
-#define ARRAY_INIT_ERROR -2
-#define COMMAND_INPUT_ERROR -3
-#define INCORRECT_CHOICE_ERROR -4
+enum{
+	OK = 0,
+	FILE_OPEN_ERROR,
+	ARRAY_INIT_ERROR,
+	COMMAND_INPUT_ERROR,
+	INCORRECT_CHOICE_ERROR,
+	INCORRECT_MATRIX_SIZE_WHILE_AMOUNT,
+	INCORRECT_MATRIX_SIZE_WHILE_MULTIP
+};
 
 struct DoubleArray
 {
 	int lines;
 	int col;
 	double **data;
-}mtrx1, mtrx2;
+}mtrx1, mtrx2, mtrx3;
+
+void MatrixPrint(struct DoubleArray *mtrx);
 
 int CommandInputChecking(int argc, char **argv);
 
@@ -21,10 +27,10 @@ int ReadingFile(FILE* f, struct DoubleArray *mtrx);
 FILE* WorkingFile(char* name, int* code);
 int ArrayInit(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, char **argv);
 
-int MatrixAmount(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2);
-int MatrixMulti(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2);
-int MatrixGaussian(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2);
-int MatrixProcessing(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, char **choice);
+int MatrixAmount(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, struct DoubleArray *mtrx3);
+int MatrixMulti(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, struct DoubleArray *mtrx3);
+int MatrixGaussian(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, struct DoubleArray *mtrx3);
+int MatrixProcessing(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, struct DoubleArray *mtrx3, char **choice);
 
 int ErrorOut(int code);
 
@@ -34,13 +40,25 @@ int main(int argc, char **argv)
 		return 1;
 	if ( ErrorOut( ArrayInit(&mtrx1, &mtrx2, argv) ) )
 		return 1;
-	if ( ErrorOut( MatrixProcessing(&mtrx1, &mtrx2, argv) ) )
+	if ( ErrorOut( MatrixProcessing(&mtrx1, &mtrx2, &mtrx3, argv) ) )
 		return 1;
-	printf("OKOK\n");
+	MatrixPrint(&mtrx3);
+	printf("\nOK\n");
 	return OK;
 }
 
 
+void MatrixPrint(struct DoubleArray *mtrx)
+{
+	for ( int i = 0; i < mtrx->lines; i++ )
+	{
+		printf("\n");
+		for ( int j = 0; j < mtrx->col; j++ )
+		{
+			printf("\t%.3lf", mtrx->data[i][j]);
+		}
+	}
+}
 
 int CommandInputChecking(int argc, char **argv)
 {
@@ -125,20 +143,72 @@ int ArrayInit(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, char **argv)
 }
 
 
-int MatrixProcessing(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, char **choice)
+int MatrixAmount(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, struct DoubleArray *mtrx3)
+{
+	int code = OK;
+	if( mtrx1->lines == mtrx2->lines && mtrx1->col == mtrx2->col )
+	{
+		mtrx3->lines = mtrx1->lines;
+		mtrx3->col = mtrx1->col;
+		code = DynamicInit(mtrx3);
+
+		for ( int i = 0; i < mtrx1->lines; i++ )
+		{
+			for ( int j = 0; j < mtrx1->col; j++ )
+			{
+				mtrx3->data[i][j] = mtrx1->data[i][j] + mtrx2->data[i][j];
+			}
+		}
+	}
+	else
+		code = INCORRECT_MATRIX_SIZE_WHILE_AMOUNT;
+
+	return code;
+}
+
+int MatrixMulti(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, struct DoubleArray *mtrx3)
+{
+	int code = OK;
+	if ( mtrx1->col == mtrx2->lines )
+	{
+		mtrx3->lines = mtrx1->lines;
+		mtrx3->col = mtrx2->col;
+		code = DynamicInit(mtrx3);
+
+		double sum = 0;
+		for (int i = 0; i < mtrx1->lines; i++)
+		{
+			for (int j = 0; j < mtrx2->col; j++)
+			{
+				for (int k = 0; k < mtrx2->lines; k++)
+				{
+					sum += mtrx1->data[i][k] * mtrx2->data[k][j];
+				}
+				mtrx3->data[i][j] = sum;
+				sum = 0;
+			}
+		}
+	}
+	else
+		code = INCORRECT_MATRIX_SIZE_WHILE_MULTIP;
+
+	return code;
+}
+
+int MatrixProcessing(struct DoubleArray *mtrx1, struct DoubleArray *mtrx2, struct DoubleArray *mtrx3, char **choice)
 {
 	int code = OK;
 	if (*choice[3] == '1')
 	{
-//		code = MatrixAmount(mtrx1, mtrx2);
+		code = MatrixAmount(mtrx1, mtrx2, mtrx3);
 	}
 	else if(*choice[3] == '2')
 	{
-//		code = MatrixMulti(mtrx1, mtrx2);
+		code = MatrixMulti(mtrx1, mtrx2, mtrx3);
 	}
 	else if(*choice[3] == '3')
 	{
-//		code = MatrixGaussian(mtrx1, mtrx2);
+//		code = MatrixGaussian(mtrx1, mtrx2, mtrx3);
 	}
 	else
 		code = INCORRECT_CHOICE_ERROR;
@@ -159,5 +229,9 @@ int ErrorOut(int code)
 		printf("Check your input parameters!\n");
 	else if(code == INCORRECT_CHOICE_ERROR)
 		printf("Check your choice parameter, you entered wrong option!\n");
+	else if(code == INCORRECT_MATRIX_SIZE_WHILE_AMOUNT)
+		printf("Matrices amount find error: You cant sum two different sized mamtrices!\n");
+	else if(code == INCORRECT_MATRIX_SIZE_WHILE_MULTIP)
+		printf("Matrices multiplicate find error: You cant multiplicate two different sized mamtrices, if columns first matrix not equal with rows second matrix!\n");
 	return code;
 }
