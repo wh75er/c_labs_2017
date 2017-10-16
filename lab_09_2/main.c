27de9faa2d6783476a8de2dbed402da7385c677b
kam16u810 <bokseng12@gmail.com>
2017-10-16 13:14:30 +0300

    New version of program have been added. Main function realized

--- lab_09_2/main.c
+++ lab_09_2/main.c
@@ -2,48 +2,121 @@
 #include <stdlib.h>
 #define SIZE_OF_BUFF 128
 
-ssize_t my_getdelim(char **lineptr, size_t *n, int delim, FILE *stream);
+enum {
+	POINTER_NULL = -10,
+	FILE_DISSAPOINT,
+	MEM_REALLOC_ERROR,
+	EMPTY_BUFFER_ERROR = -7,
 
-int main()
-{
-	setbuf(stdout, NULL);
-	char **str;
-	size_t n = 0;
-	int delim = 'f';
-	FILE* f = fopen("input.txt", "r");	
-	*str = (char*)malloc(0);
+	OK = 0,
+	ARG_INPUT_ERROR,
+	FILE_READING_ERROR,
 	
-	int result = my_getdelim(str, &n, delim, f);
-	printf("%s\n", *str);
-	printf("%d\n", result);
-	result = my_getdelim(str, &n, delim, f);
-	printf("%s\n", *str);
-	printf("%d\n", result);
-	result = my_getdelim(str, &n, delim, f);
-	printf("%s\n", *str);
-	printf("%d\n", result);
+};
+
+
+typedef struct{
+	char* fileIn;
+	char* fileOut;
+	char* replace;
+	char* search;
+}args;
+
+typedef struct{
+	FILE* file;
+	char** str;
+}string;
+
+
+ssize_t my_getdelim(char **lineptr, size_t *n, int delim, FILE *stream);
+char* strReplace(const char *source, const char *search, const char *replace);
+
+
+int argInit(const int argc, char** argv, args *args);
+int stringInit(string *text);
+int openFile(const char* fileIn, FILE** f);
+int readFile(FILE* f, char** str);
+int stringProcessing(const char *search, const char *replace, char** str);
+int writeFile(const char* fileOut, char** str);
+int isError(const int code);
+
+
+int main(int argc, char **argv)
+{
+	args args;
+	string text;
+	if(isError(argInit(argc, argv, &args)))
+		return 1;
+	if(isError(stringInit(&text)))
+		return 1;
+	if(isError(openFile(args.fileIn, &text.file)))
+		return 1;
+/*	if(isError(readFile(text.file, text.str)))
+		return 1;
+	if(isError(stringProcessing(args.search, args.replace, text.str)))
+		return 1;
+	if(isError(writeFile(args.fileOut, text.str)))
+		return 1;
+		*/
+	return 0;			
 }
+
+int openFile(const char* fileIn, FILE** f)
+{
+	*f = fopen(fileIn, "r");
+	if (!*f)
+		return FILE_READING_ERROR;
+	return OK;
+}
+
+int stringInit(string *text)
+{
+	text->file = NULL;
+	text->str = (char**)malloc(sizeof(char*));
+	*text->str = NULL;
+
+	return OK;
+}
+
+int argInit(const int argc, char** argv, args *args)
+{
+	if(argc < 4 ||  argc > 5)
+		return ARG_INPUT_ERROR;
+	args->fileIn = argv[1];
+	args->fileOut = argv[2];
+	args->search = argv[3];
+	if(argc == 3){
+		args->replace = NULL;
+	}
+	if(argc == 4){
+		args->replace = argv[4];
+	}
+
+	return OK;
+}
+
+
 
 ssize_t my_getdelim(char **lineptr, size_t *n, int delim, FILE *stream)
 {
 	if (lineptr == NULL || n == NULL)
-		return -1;
+		return POINTER_NULL;
 
 	if (stream == NULL)
-		return -1;
+		return FILE_DISSAPOINT;
 
 
 	char* array = *lineptr;
 	if(!array || *n < SIZE_OF_BUFF || !*n){
 		array = (char*)realloc(*lineptr, SIZE_OF_BUFF);
 		if (!array)
-			return -1;
+			return MEM_REALLOC_ERROR;
 		*n = SIZE_OF_BUFF;
 	}	
 	else if(array && *n){
 		array = (char*)realloc(*lineptr, *n + SIZE_OF_BUFF);
 		if (!array)
-			return -1;
+			return MEM_REALLOC_ERROR;
 		*n += SIZE_OF_BUFF;
 	}
 	*lineptr = array;
@@ -52,10 +125,10 @@ ssize_t my_getdelim(char **lineptr, size_t *n, int delim, FILE *stream)
 	int bytes = 0;
 	char buff[SIZE_OF_BUFF], *arrayOfIn, *arrayOfBuff, *pa;
 	while(fgets(buff, SIZE_OF_BUFF, stream)){
-		printf("%s-END\n", buff);
+//		printf("%s-END\n", buff);
 		for (pa = buff; pa < buff + SIZE_OF_BUFF && *pa != delim && *pa != '\0'; pa++);
 		if (pa == buff + SIZE_OF_BUFF || *pa == '\0'){
-			printf("OK\n");
+//			printf("OK\n");
 			bytes+= pa - buff;
 			arrayOfIn = *lineptr;
 			arrayOfBuff = buff;
@@ -68,24 +141,33 @@ ssize_t my_getdelim(char **lineptr, size_t *n, int delim, FILE *stream)
 			}
 		}
 		else{
-			bytes+= pa - buff;
+			bytes+= pa - buff + 1;
 			arrayOfIn = *lineptr;
 			arrayOfBuff = buff;
 			while (arrayOfBuff != buff + bytes){
 				if(*arrayOfIn == '\0'){
-					if(*arrayOfBuff != delim)
-						*arrayOfIn = *arrayOfBuff;
+					*arrayOfIn = *arrayOfBuff;
 					arrayOfBuff++;
 				}
 				arrayOfIn++;
 			}
 			char* pa_l;
 			for(pa_l = *lineptr; *pa_l != '\0'; pa_l++);
-			fseek(stream, pa_l - *lineptr + *n/128, SEEK_SET);
+			fseek(stream, pa_l - *lineptr + *n/128 - 1, SEEK_SET);
 			return bytes;
 		}
 	}
 	if (buff == 0)
-		return -1;
+		return EMPTY_BUFFER_ERROR;
 	return bytes;
+}
+
+
+int isError(const int code)
+{
+	if(code == ARG_INPUT_ERROR)
+		printf("Check your input parameters!\n");
+	if(code == FILE_READING_ERROR)
+		printf("File reading error!\n");
+	return code;
 }
