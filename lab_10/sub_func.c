acdb820be32faa7e03ba9f7ab0b4e6d5fb9d71c2
kam16u810 <bokseng12@gmail.com>
2017-11-21 18:35:17 +0300

    Functions have been added

--- lab_10/sub_func.c
+++ lab_10/sub_func.c
@@ -6,5 +6,29 @@ void* pop_front(node_t **head)
 	if(!(*head->next))
 		return NULL;
 
+	void* data = *head->data;
+	node_t *tmp = *head;
+	*head = *head->next;
+	free(tmp);
+
+	return data;
 	
+}
+
+
+void insert(node_t **head, node_t *elem, node_t *before)
+{
+	if(before == *head) {
+		elem->next = *head;
+		*head = elem;
+	}
+	
+	node_t tmp;
+	tmp.next = before->next;
+	tmp.data = before->data;
+	before->next = elem;
+	before->data = elem->data;
+	
+	elem->data = tmp.data;
+	elem->next = tmp.next;
 }
