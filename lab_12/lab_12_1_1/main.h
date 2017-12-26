22de922251be9965f18e864b9791de4a161560d9
wh75er <bokseng12@gmail.com>
2017-12-26 08:51:37 +0000

    lab_07 cpy

---
+++ lab_12/lab_12_1_1/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include "counter.h"
+#include "dynamic_arrays.h"
+#include "swap.h"
+#include "libs.h"
+#include "compare.h"
+#include "sort_filter.h"
+
+#include "file_open.h"
+#include "main_dynamic_array.h"
+#include "file_write.h"
+
+#endif
