8b0f612b85781289d3777165c13f84ce26982e0f
wh75er <bokseng12@gmail.com>
2017-12-26 06:58:41 +0000

    if changed

--- lab_11/func.c
+++ lab_11/func.c
@@ -6,7 +6,7 @@ int my_snprintf(char *str, size_t size, const char *format, ...)
 	va_start(ap, format);
 	int count = 0; 
 
-	if(!str)
+	if(!str && !format)
 		return count;
 
 	int flag = 0;
@@ -17,65 +17,64 @@ int my_snprintf(char *str, size_t size, const char *format, ...)
 			break;
 		}
 
-		if(!flag && *pa == '%') {
-			flag = 1;
-		}
-		else if(!flag) {
-			if(count < size) {
-				*strP = *pa;
-				strP++;
-				count++;
-			}
-		}
-
-		if(flag && *pa == 's') {
-			char *tmp = va_arg(ap, char*);
-			for(char* pb = tmp; *pb != '\0'; pb++) {
+		if(!flag) {
+			if(*pa == '%')
+				flag = 1;
+			else 
 				if(count < size) {
-					*strP = *pb;
-					strP++;
-					count++;
-				}
-				count++;
-			}
-			flag = 0;
-		}
-		else if(flag && *pa == 'c') {
-			char tmp = (long int)va_arg(ap, int);
-			if(count < size) {
-				*strP = (char)tmp;
-				strP++;
-				count++;
-			}
-			flag = 0;
-		}
-		else if(flag && *pa == 'd') {
-			long int tmp = (long int)va_arg(ap, int);
-			if(count < size) {
-				int2str(&strP, &count, size, tmp);
-			}
-			flag = 0;
-		}
-		else if(flag && *pa == '%') {
-			if(count < size && pa > format) {
-				if(*(pa-1) == '%') {
 					*strP = *pa;
 					strP++;
 					count++;
-					flag = 0;
 				}
+		}
+
+		if(flag) {
+			if(*pa == 's') {
+				char *tmp = va_arg(ap, char*);
+				for(char* pb = tmp; *pb != '\0'; pb++) {
+					if(count < size) {
+						*strP = *pb;
+						strP++;
+						count++;
+					}
+					count++;
+				}
+				flag = 0;
+			}
+			else if(*pa == 'c') {
+				char tmp = (char)va_arg(ap, int);
+				if(count < size) {
+					*strP = (char)tmp;
+					strP++;
+					count++;
+				}
+				flag = 0;
+			}
+			else if(*pa == 'd') {
+				long int tmp = (long)va_arg(ap, long);
+				if(count < size) {
+					int2str(&strP, &count, size, tmp);
+				}
+				flag = 0;
+			}
+			else if(*pa == '%' && count < size && pa > format && *(pa-1) == '%') {
+						*strP = *pa;
+						strP++;
+						count++;
+						flag = 0;
+			}
+			else if(*pa == 'x') {
+				unsigned long int tmp = (unsigned long int)va_arg(ap, unsigned int);
+				hex2str(&strP, &count, size, tmp);
+				flag = 0;
 			}
 		}
-		else if(flag && *pa == 'x') {
-			unsigned long int tmp = (unsigned long int)va_arg(ap, unsigned int);
-			hex2str(&strP, &count, size, tmp);
-			flag = 0;
-		}
 	}
+
 	if(count + 1 < BUFFSIZE) {
 		*strP = '\0';
 	}
-	*(str + BUFFSIZE) = '\0';
+	*(str + BUFFSIZE - 1) = '\0';
 	va_end(ap);
 	return count;
 }
